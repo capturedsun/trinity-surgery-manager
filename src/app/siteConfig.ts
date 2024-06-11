31e4ccc2ca1b3b0d6665a3504fa440f1386da32a
metacryst <samrussell99@gmail.com>
2024-06-11 15:34:17 -0500

    init

---
+++ src/app/siteConfig.ts
@@ -0,0 +1,17 @@
+export const siteConfig = {
+  name: "Dashboard",
+  url: "https://dashboard.tremor.so",
+  description: "The only dashboard you will ever need.",
+  baseLinks: {
+    home: "/",
+    overview: "/overview",
+    patients: "/patients",
+    settings: {
+      general: "/settings/general",
+      billing: "/settings/billing",
+      users: "/settings/users",
+    },
+  },
+}
+
+export type siteConfig = typeof siteConfig
---
+++ src/data/schema.ts
@@ -0,0 +1,20 @@
+export type Usage = {
+  name: string
+  status: string
+  costs: number
+  surgery: string
+  facility: number
+  date: string
+}
+
+export type OverviewData = {
+  date: string
+  "Rows written": number
+  "Rows read": number
+  Queries: number
+  "Payments completed": number
+  "Sign ups": number
+  Logins: number
+  "Sign outs": number
+  "Support calls": number
+}
